1a0da76fa9a0c9044d1fb3e0fefa28e401ce6784
liuky3 <specter119@gmail.com>
2026-01-06 09:46:19 +0800

    cleanup previous dotfiles

--- julia/config/startup.jl
+++ julia/config/startup.jl
@@ -1,5 +1,12 @@
 #!/usr/bin/env julia
 ENV["JULIA_PKG_SERVER"] = "https://mirrors.sjtug.sjtu.edu.cn/julia"
-ENV["CONDA_JL_HOME"] = "/"
-ENV["PYTHON"] = "$(ENV["HOME"])/.conda/envs/julia/bin/python"
+
+# conda.jl
+ENV["CONDA_JL_HOME"] = ENV["MAMBA_ROOT_PREFIX"] # * "\\envs\\jupyverse"
+ENV["CONDA_JL_CONDA_EXE"] = ENV["MAMBA_EXE"]
+
+# IJulia
 ENV["JUPYTER"] = "jupyter"
+
+## PyCall.jl
+# ENV["PYTHON"] = ENV["MAMBA_ROOT_PREFIX"] # * "\\envs\\jupyverse\\bin\\python"
--- vscode/fish/functions/code.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env fish
-function code --wraps=code-insiders --description 'alias code-insiders as code'
-    if type -q code-insiders
-        code-insiders $argv
-    else
-        command code $argv
-    end
-end
