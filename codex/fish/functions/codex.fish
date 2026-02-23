6d55d8f3ff886fcd5b342056e5fa1cba5942c7cf
liuky3 <liuky3@oocl.com>
2026-02-23 11:15:45 +0800

    make claude and codex shares the common function

--- codex/fish/functions/codex.fish
+++ codex/fish/functions/codex.fish
@@ -1,57 +1,10 @@
 function codex --description 'Run codex with bwrap sandboxing'
-    # Purpose: Add security isolation using bwrap sandboxing
-    # Simple setup: just mount ~/.codex directory
-
     set -q CODEX_CONFIG; or set CODEX_CONFIG $HOME/.config/codex
     set CODEX_CONFIG (string replace '~' $HOME $CODEX_CONFIG)
 
-    # Create codex home directory
     mkdir -p $CODEX_CONFIG
 
-    # Detect git worktree and prepare extra mounts
-    set -l worktree_mounts
-    if functions -q git_worktree_mounts
-        set worktree_mounts (git_worktree_mounts)
-    end
-
-    # Prepare read-only reference mounts from BWRAP_RO_REFS
-    set -l ro_ref_mounts
-    if functions -q bwrap_ro_ref_mounts
-        set ro_ref_mounts (bwrap_ro_ref_mounts)
-    end
-
-    # Bwrap sandboxing
-    bwrap \
-        --dev /dev \
-        --dev-bind-try /dev/dri /dev/dri \
-        --proc /proc \
-        --ro-bind /etc /etc \
-        --ro-bind /usr/share /usr/share \
-        --ro-bind /usr/lib /usr/lib \
-        --ro-bind /usr/lib64 /usr/lib64 \
-        --ro-bind /usr/bin /usr/bin \
-        --ro-bind /usr/sbin /usr/sbin \
-        --ro-bind /usr/include /usr/include \
-        --bind /tmp /tmp \
-        --bind /run /run \
-        --bind /var /var \
-        --ro-bind-try /opt /opt \
-        --symlink /usr/lib /lib \
-        --symlink /usr/lib /lib64 \
-        --symlink /usr/bin /bin \
-        --symlink /usr/bin /sbin \
-        --tmpfs $HOME \
-        --dir $HOME/.config \
-        --bind-try $HOME/.config $HOME/.config \
-        --dir $HOME/.local \
-        --bind-try $HOME/.local $HOME/.local \
-        --dir $HOME/.cache \
-        --bind-try $HOME/.cache $HOME/.cache \
+    _bwrap_agentic_cli \
         --bind $CODEX_CONFIG $HOME/.codex \
-        $ro_ref_mounts \
-        --bind (pwd) (pwd) \
-        $worktree_mounts \
-        --setenv HOME $HOME \
-        --setenv PATH $HOME/.local/bin:/usr/bin:/bin:/usr/sbin:/sbin \
-        (command -v codex) $argv
+        -- codex $argv
 end
